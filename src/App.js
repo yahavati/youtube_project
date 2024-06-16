e94360bab4cedce855dd38b47d27aaec4c86c486
yahav atias <yahavatias13@gmail.com>
2024-06-16 22:58:41 +0300

    Signup commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// File: src/App.js
 
-function App() {
+import React from 'react';
+import SignUp from './Sign_up/SignUp'; 
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SignUp />
     </div>
   );
-}
+};
 
 export default App;
